7cea1abd6d7acaabae391d0f386e456ec54822d2
alexandre.silva <luiz.silva@fpf.br>
2020-11-12 18:31:20 -0400

    setup & lint config

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const PORT = 3000
+const app = express()
+
+app.use(express.json())
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
